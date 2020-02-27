b5eebbb80aa264e9e33c51e19d71e85fa3e9b710
Rushabh Prajapati <rushabh@quarkstudios.com>
2020-02-27 12:36:28 +0530

    - Add gson library and add data read function in util class

---
+++ app/src/main/java/com/example/obvious_rushabh/vm/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.obvious_rushabh.vm
+
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.obvious_rushabh.model.NasaModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
+import java.io.InputStream
+
+class MainViewModel : ViewModel() {
+
+    fun getJsonDataFromAsset(context: Context, fileName: String): List<NasaModel>? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        val gson = Gson()
+        val listPersonType = object : TypeToken<List<NasaModel>>() {}.type
+        val nasaModel: List<NasaModel> = gson.fromJson(jsonString, listPersonType)
+        nasaModel.forEachIndexed { idx, nasa -> Log.i("data", "> Item $idx:\n$nasa") }
+
+        return nasaModel
+    }
+
+
+}
